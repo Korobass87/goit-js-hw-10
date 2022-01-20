6f27819db29f31534c9bf8d9e8baf6a58f596cb3
Alexander Korobov <korobass1987@yandex.ru>
2022-01-20 16:59:44 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,87 @@
+// import { zip } from 'lodash';
+import Notiflix from 'notiflix'
+Notiflix.Notify.init({
+  
+  position: 'center-top',
+  distance: '10px',
+  opacity: 1,
+  // ...
+});
 import './css/styles.css';
-
+const debounce = require('lodash.debounce')
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector("#search-box")
+const listInput = document.querySelector('.country-list')
+const info = document.querySelector('.country-info')
+let oneName = ""
+
+
+// fetch(`https:restcountries.com/v2/name/russia?fields=capital,name,population,flags,languages`)
+// .then(x=>x.json())
+//     .then(console.log)
+
+input.addEventListener("input", debounce(searchCountry, DEBOUNCE_DELAY))
+
+function searchCountry() {
+    let dataSearch = input.value.trim()
+    
+    function onFetch() {
+        return fetch(`https:restcountries.com/v3.1/name/${dataSearch}?fields=capital,name,population,flags,languages`)
+    }
+
+    if (dataSearch) {
+        onFetch()
+            .then(d => {
+                
+                return d.json()
+            })
+            .then(data => {
+                if (data.status === 404) {
+                    Notiflix.Notify.failure("Oops, there is no country with that name")
+                    info.innerHTML = ''
+                    listInput.innerHTML = ''
+                } else {
+                    return markup(data)
+                }
+                
+            })
+    } else {listInput.innerHTML = ''
+            info.innerHTML = '' }
+     
+}
+
+
+function markup(data) {
+    
+    if (data.length !== 1 && data.length < 11) {
+     Notiflix.Notify.success(`Find ${data.length} matches`)   
+     let  listMarkup =  data.map(country => `<li class="country-item">
+    <img class="flag-name" src="${country.flags.svg}" alt="${country.name.common}">
+    ${country.name.official}
+    </li>` ).join("")
+        
+        listInput.innerHTML = ''    
+        info.innerHTML = ''
+        listInput.insertAdjacentHTML('beforeend', listMarkup)
+    } else if ((data.length > 10)) {
+        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+        info.innerHTML = ''
+        listInput.innerHTML = ''
+        
+    } else {
+        let leng = Object.values(data[0].languages).join(', ')
+        
+        
+        let infoMarkup = data.map(country => `<h2><img class="flag-name" src="${country.flags.svg}" alt="${country.name.common}">${country.name.official}</h2>
+        <ul><li class="country-info-item"><span class="wrap">Capital: </span>${country.capital[0]}</li>
+        <li class="country-info-item"><span class="wrap">Population: </span>${country.population}</li>
+        <li class="country-info-item"><span class="wrap">Languages: </span>${leng}</li>
+        </ul>`).join('')
+        // let infoMarkup = data.map()     
+            listInput.innerHTML = ''
+            info.innerHTML = ''
+            info.insertAdjacentHTML('beforeend', infoMarkup)
+        
+
+    }
+}
